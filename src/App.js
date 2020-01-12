838d0a886063088ca646da19088e5965feb2e1ee
Duviel Garcia <duviel7@gmail.com>
2020-01-11 20:46:55 -0500

    Componentes de canción e información de lartista, se almacena los datos en el state y se muestra en cada componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Informacion from './components/Informacion';
 import axios from 'axios';
 
 function App () {
   
   // state 
   const [ artista, agregarArtista ] = useState('');
-  const [ letra, agregarLista] = useState([]);
+  const [ letra, agregarLetra] = useState([]);
   const [ info, agregarInfo ] = useState({});
 
   // Consultar API de letras de caciones
@@ -17,14 +19,48 @@ function App () {
     //consulta a la api 
     const resultado = await axios(url);
 
-    console.log(resultado.data.lyrics);
+    // Almacenar los datos en en el state
+    agregarArtista(artista);
+    agregarLetra(resultado.data.lyrics);
   }
+
+  // Consultar la API para la información del artisata
+  const consultarAPIInfo = async () => {
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+    const resultado = await axios(url);
+    agregarInfo(resultado.data.artists[0]);
+  }
+
+  useEffect(
+    () => {
+      if(artista){
+        consultarAPIInfo();
+      }
+      
+    }, [artista]
+  );
 
   return(
     <Fragment>
       <Formulario
         consultarApiLetra={consultarApiLetra}
       />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Informacion 
+              info={info}
+            />
+          </div>
+          <div className="col-md-6">
+            <Cancion 
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
