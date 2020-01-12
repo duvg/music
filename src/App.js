f929c9f38582b7434bf83339507ca1fce5418ff3
Duviel Garcia <duviel7@gmail.com>
2020-01-11 19:22:42 -0500

    Componente formulario para buscar las letras de las canciones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App () {
-  return(<p>Start App</p>);
+  
+  // state 
+  const [ artista, agregarArtista ] = useState('');
+  const [ letra, agregarLista] = useState([]);
+  const [ info, agregarInfo ] = useState({});
+
+
+  return(
+    <Fragment>
+      <Formulario />
+    </Fragment>
+  );
 }
 
 export default App;
